6f0091b0d481bdde33a2b04e59c7292943c80cda
lburgazzoli <lburgazzoli@gmail.com>
2014-11-06 22:55:34 +0100

    IPC Example 1

--- chronicle-examples/chronicle-queue/src/main/java/com/github/lburgazzoli/openhft/examples/chronicle/queue/IpcExample1.java
+++ chronicle-examples/chronicle-queue/src/main/java/com/github/lburgazzoli/openhft/examples/chronicle/queue/IpcExample1.java
@@ -22,71 +22,72 @@ import net.openhft.chronicle.IndexedChronicle;
 import net.openhft.chronicle.tcp.ChronicleSink;
 import net.openhft.chronicle.tcp.ChronicleSource;
 import net.openhft.lang.io.Bytes;
+import net.openhft.lang.io.IOTools;
 import net.openhft.lang.io.serialization.BytesMarshallable;
 import net.openhft.lang.model.constraints.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.File;
 import java.io.IOException;
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
-public class IpcExampleMain {
+public class IpcExample1 {
 
     // *************************************************************************
     //
     // *************************************************************************
 
     private static class Msg implements BytesMarshallable {
-        public long longField;
-        public int intField;
+        public int index;
+        public String data;
 
         public Msg() {
-            this(0,0);
+            this(0, "");
         }
 
-        public Msg(long longField, int intField) {
-            this.longField = longField;
-            this.intField = intField;
+        public Msg(int index, String data) {
+            this.index = index;
+            this.data = data;
         }
 
-        public long longField() {
-            return this.longField;
+        public int index() {
+            return this.index;
         }
 
-        public Msg longField(long longField) {
-            this.longField = longField;
+        public Msg index(int index) {
+            this.index = index;
             return this;
         }
 
-        public int intField() {
-            return this.intField;
+        public String data() {
+            return this.data;
         }
 
-        public Msg intField(int intField) {
-            this.intField = intField;
+        public Msg data(String data) {
+            this.data = data;
             return this;
         }
 
         @Override
         public void readMarshallable(@NotNull Bytes in) throws IllegalStateException {
-            longField = in.readLong();
-            intField  = in.readInt();
+            index = in.readInt();
+            data  = in.readUTFΔ();
         }
 
         @Override
         public void writeMarshallable(@NotNull Bytes out) {
-            out.writeLong(longField);
-            out.writeInt(intField);
+            out.writeInt(index);
+            out.writeUTFΔ(data);
         }
 
         @Override
         public String toString() {
-            return "{" +
-                " long=" + longField +
-                " int="  + intField  +
+            return "Msg {" +
+                " index="   + this.index +
+                ", data="   + this.data  +
             " }";
         }
     }
@@ -100,59 +101,12 @@ public class IpcExampleMain {
 
         private final String basePath;
         private final int nbMessages;
+        private final CountDownLatch latch;
 
-        public P1(@NotNull String basePath, int nbMessages) {
+        public P1(@NotNull String basePath, int nbMessages, CountDownLatch latch) {
             this.basePath = basePath;
             this.nbMessages = nbMessages;
-        }
-
-        @Override
-        public void run() {
-            try {
-                Chronicle out = new IndexedChronicle(basePath + "/queue-out");
-                Chronicle in  = new IndexedChronicle(basePath + "/queue-in");
-
-                ExcerptAppender writer = out.createAppender();
-                ExcerptTailer reader = in.createTailer();
-
-                final Msg msg = new Msg();
-                for(int i=0;i<nbMessages;i++) {
-                    writer.writeInstance(
-                        Msg.class,
-                        msg.longField(1000 + i).intField(i)
-                    );
-
-                    while (true) {
-                        if (reader.nextIndex()) {
-                            reader.readInstance(Msg.class, msg);
-                            reader.finish();
-
-                            LOGGER.info("received {}", msg);
-                            break;
-                        }
-                    }
-                }
-
-                writer.close();
-                reader.close();
-
-                out.close();
-                in.close();
-            } catch(IOException e) {
-                LOGGER.warn("", e);
-            }
-        }
-    }
-
-    private static final class P2 implements Runnable {
-        private static final Logger LOGGER = LoggerFactory.getLogger(P2.class);
-
-        private final String basePath;
-        private final int nbMessages;
-
-        public P2(@NotNull String basePath, int nbMessages) {
-            this.basePath = basePath;
-            this.nbMessages = nbMessages;
+            this.latch = latch;
         }
 
         @Override
@@ -167,20 +121,46 @@ public class IpcExampleMain {
                     "localhost",
                     10002);
 
+                ExcerptAppender writer = publisher.createAppender();
                 ExcerptTailer reader = subscriber.createTailer();
-                for(int i=0; i<nbMessages; ) {
-                    if(reader.nextIndex()) {
-                        LOGGER.info("Got a message");
-                        i++;
 
-                        reader.finish();
+                Msg msg = new Msg();
+                for(int i=0; i<nbMessages && !Thread.interrupted(); i++) {
+                    msg.index(i);
+                    msg.data("data-" + i);
+
+                    writer.startExcerpt();
+                    writer.writeInstance(Msg.class, msg);
+                    writer.finish();
+
+                    while (!Thread.interrupted()) {
+                        if (reader.nextIndex()) {
+                            reader.readInstance(Msg.class, msg);
+                            reader.finish();
+
+                            if(msg.index() != i) {
+                                LOGGER.warn("index : expected {}, got {}", i, msg.index());
+                            }
+                            if(!msg.data().equals("data-" + i)) {
+                                LOGGER.warn("data  : expected {}, got {}", "data-" + i, msg.data());
+                            }
+
+                            break;
+                        }
                     }
                 }
 
+                LOGGER.info("Done");
+
+                writer.close();
                 reader.close();
-                subscriber.close();
+
+                this.latch.countDown();
+                this.latch.await(30, TimeUnit.SECONDS);
+
                 publisher.close();
-            } catch(IOException e) {
+                subscriber.close();
+            } catch(Exception e) {
                 LOGGER.warn("", e);
             }
         }
@@ -191,10 +171,12 @@ public class IpcExampleMain {
 
         private final String basePath;
         private final int nbMessages;
+        private final CountDownLatch latch;
 
-        public P3(@NotNull String basePath, int nbMessages) {
+        public P3(@NotNull String basePath, int nbMessages, CountDownLatch latch) {
             this.basePath = basePath;
             this.nbMessages = nbMessages;
+            this.latch = latch;
         }
 
         @Override
@@ -210,19 +192,43 @@ public class IpcExampleMain {
                     10002);
 
                 ExcerptTailer reader = subscriber.createTailer();
-                for(int i=0; i<nbMessages; ) {
-                    if(reader.nextIndex()) {
-                        LOGGER.info("Got a message");
-                        i++;
+                ExcerptAppender writer = publisher.createAppender();
 
+                Msg msg = new Msg();
+                for(int i=0; i<nbMessages && !Thread.interrupted(); ) {
+                    if(reader.nextIndex()) {
+                        reader.readInstance(Msg.class, msg);
                         reader.finish();
+
+                        if(msg.index() != i) {
+                            LOGGER.warn("index : expected {}, got {}", i, msg.index());
+                        }
+                        if(!msg.data().equals("data-" + i)) {
+                            LOGGER.warn("data  : expected {}, got {}", "data-" + i, msg.data());
+                        }
+
+                        msg.index(i);
+                        msg.data("data-" + i);
+
+                        writer.startExcerpt();
+                        writer.writeInstance(Msg.class, msg);
+                        writer.finish();
+
+                        i++;
                     }
                 }
 
                 reader.close();
+                writer.close();
+
+                LOGGER.info("Done");
+
+                this.latch.countDown();
+                this.latch.await(30, TimeUnit.SECONDS);
+
                 subscriber.close();
                 publisher.close();
-            } catch(IOException e) {
+            } catch(Exception e) {
                 LOGGER.warn("", e);
             }
         }
@@ -233,18 +239,19 @@ public class IpcExampleMain {
     // *************************************************************************
 
     public static void main(final String[] args) throws Exception {
-        final Logger logger     = LoggerFactory.getLogger(IpcExampleMain.class);
-        final String basePath   = "data/chronicle-ipc"; //System.getProperty("java.io.tmpdir") + "/chronicle-ipc";
-        final int    nbMessages = 10;
+        final String basePath = System.getProperty("java.io.tmpdir") + "/chronicle-ipc";
+        final int nbMessages = 1000;
+        final CountDownLatch latch = new CountDownLatch(2);
 
-        new File(basePath).delete();
+        IOTools.deleteDir(basePath);
 
         ExecutorService svc = Executors.newFixedThreadPool(3);
-        //svc.execute(new P3(basePath, nbMessages));
-        //svc.execute(new P2(basePath, nbMessages));
-        svc.execute(new P1(basePath, nbMessages));
+        svc.execute(new P3(basePath, nbMessages, latch));
+        svc.execute(new P1(basePath, nbMessages, latch));
 
-        svc.shutdownNow();
-        svc.awaitTermination(1, TimeUnit.MINUTES);
+        latch.await(3, TimeUnit.MINUTES);
+
+        svc.shutdown();
+        svc.awaitTermination(30, TimeUnit.SECONDS);
     }
 }
