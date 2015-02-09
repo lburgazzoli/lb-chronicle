9348eccff4a7d64c63bbd400f4bf738b5e02fac6
lburgazzoli <lburgazzoli@gmail.com>
2015-02-09 14:14:44 +0100

    examples

--- chronicle-examples/chronicle-logger-logback/src/main/java/com/github/lburgazzoli/openhft/examples/chronicle/logger/logback/SimpleExample.java
+++ chronicle-examples/chronicle-logger-logback/src/main/java/com/github/lburgazzoli/openhft/examples/chronicle/logger/logback/SimpleExample.java
@@ -16,7 +16,6 @@
 package com.github.lburgazzoli.openhft.examples.chronicle.logger.logback;
 
 import net.openhft.chronicle.ChronicleQueueBuilder;
-import net.openhft.chronicle.logger.ChronicleLogEvent;
 import net.openhft.chronicle.logger.tools.ChroniTool;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -27,25 +26,13 @@ public class SimpleExample {
 
     public static void main(final String[] args) throws IOException {
         Logger log = LoggerFactory.getLogger(SimpleExample.class);
-        log.info("message", Thread.currentThread().getName() , new IOException("test"));
+        log.info("message", Thread.currentThread().getName() , Thread.currentThread().getName(), new IOException("test"));
+        log.info("message", Thread.currentThread().getName() , Thread.currentThread().getName());
 
         ChroniTool.process(
             ChronicleQueueBuilder.vanilla(
                 System.getProperty("java.io.tmpdir") + "/logger-logback").build(),
-            new ChroniTool.BinaryProcessor() {
-                @Override
-                public void process(ChronicleLogEvent event) {
-                    System.out.printf("%s|%s|%s|%s|%s - <%s><%s>\n",
-                        ChroniTool.DF.format(event.getTimeStamp()),
-                        event.getLevel().toString(),
-                        event.getThreadName(),
-                        event.getLoggerName(),
-                        event.getMessage(),
-                        event.getArgumentArray().toString(),
-                        event.getThrowable().toString()
-                    );
-                }
-            },
+            ChroniTool.READER_BINARY,
             false,
             false);
     }
